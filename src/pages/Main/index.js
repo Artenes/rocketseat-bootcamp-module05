5dce4877ae5eb40e7dd17c1556f89803f7944d94
Artenes Nogueira <artenesama@gmail.com>
2019-09-14 10:09:08 -0400

    chore: Changes Detalhes to Details

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -87,7 +87,7 @@ export default class Main extends Component {
             <li key={repository.name}>
               <span>{repository.name}</span>
               <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
-                Detalhes
+                Details
               </Link>
             </li>
           ))}
