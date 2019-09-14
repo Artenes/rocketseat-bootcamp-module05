9899efe8956186d0ae3ab5ff5a1033a358358515
Artenes Nogueira <artenesama@gmail.com>
2019-09-14 14:58:57 -0400

    Updates

---
+++ precache-manifest.4f3071243b7838e2e2b01ba1f02fdf40.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d369886780fd7e06aed3d04403de7d8",
+    "url": "/Artenes/rocketseat-bootcamp-module05/index.html"
+  },
+  {
+    "revision": "dfa129be72385523d754",
+    "url": "/Artenes/rocketseat-bootcamp-module05/static/js/2.d8b01678.chunk.js"
+  },
+  {
+    "revision": "db077ea24f77cfdf0ca0",
+    "url": "/Artenes/rocketseat-bootcamp-module05/static/js/main.f4611ef1.chunk.js"
+  },
+  {
+    "revision": "feb1f147b400cbc18c84",
+    "url": "/Artenes/rocketseat-bootcamp-module05/static/js/runtime~main.e408f8d4.js"
+  }
+]);
