fbb1b2a5178fe999a80c1733a01c1a7c9df34bc6
Artenes Nogueira <artenesama@gmail.com>
2019-09-14 10:07:54 -0400

    feat: Shows issues list

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -23,7 +23,7 @@ export const Owner = styled.header`
     text-decoration: none;
     position: absolute;
     left: 0px;
-    padding: 5px;
+    padding: 10px;
     border-radius: 5px;
 
     svg {
@@ -49,5 +49,65 @@ export const Owner = styled.header`
     line-height: 1.4;
     text-align: center;
     max-width: 400px;
+  }
+`;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover {
+            color: #7159c1;
+          }
+        }
+
+        span {
+          background: #eee;
+          color: #333;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 3px 4px;
+          margin-left: 10px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 12px;
+        color: #999;
+      }
+    }
   }
 `;
